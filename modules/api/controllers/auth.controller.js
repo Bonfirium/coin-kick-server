c165a3d9898f1207df34bc80d525c5b7dba5aaea
NickLatkovich <nicklatkovich@gmail.com>
2018-09-22 10:04:03 +0300

    Sign-out. Sign-in. Fix route GET /api/auth/me

--- modules/api/controllers/auth.controller.js
+++ modules/api/controllers/auth.controller.js
@@ -4,11 +4,16 @@ import { STATUS_CODE } from '../api.constants';
 import { addRestHandler } from '../api.module';
 import FormError from '../errors/form.error';
 import { auth, onlyLogged } from '../forms/auth.form';
+import UserModel from '../../../models/user.model';
 import { createUser, errors } from '../../../services/user.service';
+import RestError from '../errors/rest.error';
+import bcrypt from 'bcrypt';
 
 export function init() {
 	addRestHandler('post', '/api/auth/sign-up', auth, signUp);
 	addRestHandler('get', '/api/auth/me', onlyLogged, getCurrentUser);
+	addRestHandler('get', '/api/auth/sign-out', onlyLogged, signOut);
+	addRestHandler('post', '/api/auth/sign-in', auth, signIn);
 }
 
 async function signUp({ form: { email, password }, req }) {
@@ -29,5 +34,17 @@ async function signUp({ form: { email, password }, req }) {
 }
 
 function getCurrentUser({ user }) {
+	return UserModel.findById(user);
+}
+
+function signOut({ req }) {
+	req.logout();
+}
+
+async function signIn({ form: { email, password }, req }) {
+	const error = new RestError('email or password is invalid', STATUS_CODE.UNPROCESSABLE_ENTITY);
+	const user = await UserModel.findOne({ email });
+	if (!user || !await bcrypt.compare(password, user.password_hash)) throw error;
+	await promisify((cb) => req.login(user, cb))();
 	return user;
 }
--- services/user.service.js
+++ services/user.service.js
@@ -6,9 +6,11 @@ export const errors = {
 	ALREADY_EXISTS: 'ALREADY_EXISTS',
 };
 
+export const salt = 13;
+
 export function createUser(email, password) {
 	return execIndependent(async () => {
 		if (await model.findOne({ email })) throw new Error(errors.ALREADY_EXISTS);
-		return model.create({ email, password_hash: await bcrypt.hash(password, 10) });
+		return model.create({ email, password_hash: await bcrypt.hash(password, salt) });
 	});
 }
